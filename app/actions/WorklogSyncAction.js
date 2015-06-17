1b5ff7b48e73241a983d31d438f42bdbb5203fc9
Stefan Mayer <s.mayer@netconomy.net>
2015-06-17 22:33:01 +0200

    Getting worklogs after toggl sync

--- app/actions/WorklogSyncAction.js
+++ app/actions/WorklogSyncAction.js
@@ -6,21 +6,23 @@
 const request = require('superagent-bluebird-promise');
 const ActionTypes = require('../constants/ActionTypes');
 const Time = require('../utils/TimeHelper');
+const GetWorklogAction = require('./GetWorklogAction');
 
 module.exports = function LoginAction (context, payload) {
     const date = Time.getDateFromParam(payload);
     const params = `start=${Time.getDateForApi(date)}`
         + `&end=${Time.getDateForApi(date)}`;
 
+    context.dispatch(ActionTypes.SYNC_WORKLOGS);
     return request.post(`/api/toggl/sync?${params}`)
         .withCredentials()
         .then((response) => {
             if (response.status >= 200 && response.status < 300) {
-                context.dispatch(ActionTypes.LOGIN_SUCCESS, response.body);
+                context.executeAction(GetWorklogAction, date);
             } else {
-                context.dispatch(ActionTypes.LOGIN_FAILURE, response.body.message);
+                context.dispatch(ActionTypes.SYNC_WORKLOGS_FAILURE, response.body.message);
             }
         }, (error) => {
-            context.dispatch(ActionTypes.LOGIN_FAILURE, error.body.message);
+            context.dispatch(ActionTypes.SYNC_WORKLOGS_FAILURE, error.body.message);
         });
 };
